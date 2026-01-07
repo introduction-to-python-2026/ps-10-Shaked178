5d89a285a0edf5c543bd195b1a5a77522b8dd954
Shaked178 <aesgbcr@gmail.com>
2026-01-07 21:23:14 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -4,22 +4,25 @@ from scipy.signal import convolve2d
 
 
 def load_image(image_path):
-     img = Image.open(image_path)
-     return np.array(img)
+   MIKI_path = "/content/MIKIֹֹ10.png"
+   MIKI = Image.open(MIKI_path)
+   MIKI = np.array(MIKI)
+print("Image shape:", MIKI.shape )
+print("Image data type:", MIKI.dtype)
 
 
-image_array = load_image('/content/MIKIֹֹ10.png')
-print(f"Image shape: {image_array.shape}")
-print(f"Image data type: {image_array.dtype}")
 
 
 def edge_detection(image):
-    if image_array.shape[-1] == 4:
+    if image_array.ndim == 3 and image_array.shape[-1] == 4:
       gray_image = np.mean(image_array[:, :, :3], axis=2)
-  else:
+  elif image_array.ndim == 3 and image_array.shape[-1] == 3:
       gray_image = np.mean(image_array, axis=2)
+  elif image_array.ndim == 2: # Already grayscale
+      gray_image = image_array
+  else:
+      raise ValueError("Unsupported image array format for grayscale conversion.")
 
-  
   kernelY = np.array([
       [1, 2, 1],
       [0, 0, 0],
@@ -32,15 +35,19 @@ def edge_detection(image):
       [-1, 0, 1]
   ])
 
-  edgeY = convolve2d(gray_image, kernelY, mode='same', boundary='symm') 
-  edgeX = convolve2d(gray_image, kernelX, mode='same', boundary='symm') 
+  
+  edgeY = convolve2d(gray_image, kernelY, mode='same', boundary='symm')
+  edgeX = convolve2d(gray_image, kernelX, mode='same', boundary='symm')
 
   
   edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
 
   return edgeMAG
 
+edge_magnitude_image = edge_detection(MIKI)
 
-edge_magnitude_image = edge_detection(image_array)
-
-print(edge_magnitude_image)
+plt.figure(figsize=(8, 6))
+plt.imshow(edge_magnitude_image, cmap='gray') 
+plt.title('Edge Detected Miki')
+plt.axis('off')
+plt.show()
