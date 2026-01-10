0acbf1e12de544a46107f15e89a4d23dba0b3d23
Shaked178 <aesgbcr@gmail.com>
2026-01-10 22:02:15 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -2,24 +2,25 @@ import numpy as np
 from PIL import Image
 from scipy.signal import convolve2d 
 
+
+
 def load_image(image_path):
     img = Image.open(image_path)
     img_array = np.array(img)
     return img_array
 
+#נותנת לנו את מערך הנתונים
+
 def edge_detection(image):
-   
     if image.ndim == 3 and image.shape[-1] == 4:
-       
         gray_image = np.mean(image[:, :, :3], axis=2)
     elif image.ndim == 3 and image.shape[-1] == 3:
-        
         gray_image = np.mean(image, axis=2)
     elif image.ndim == 2:
-       
         gray_image = image
     else:
         raise ValueError("Unsupported image array format for grayscale conversion.")
+
 
     
     kernelY = np.array([
@@ -39,6 +40,7 @@ def edge_detection(image):
     edgeX = convolve2d(gray_image, kernelX, mode='same', boundary='symm')
 
     
+#יוצרת לנו תמונה אבל רק של הקצוות    
     edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
 
     return edgeMAG
